dac4523f1dfd7e1130ef34c2f1a3b19ba662f847
witsanu <phai.namikaze@gmail.com>
2024-08-09 14:13:25 +0000

    lab day1

---
+++ lab5.py
@@ -0,0 +1,8 @@
+myMixedTypeList = [45,290578,1.02,True,"My dog in on the bed.","45"]
+
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
+    
+myVal = "'test'"
+print(myVal)
+print(type(myVal))
